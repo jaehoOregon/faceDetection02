70371c9066c9c873da9f76a9954366fd04b50671
JaehoBuildiOSApp <jaeho.jung.01@gmail.com>
2023-01-21 15:37:46 +0900

    Initial Commit

---
+++ faceDetection02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faceDetection02
+//
+//  Created by Jaeho Jung on 2023/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
